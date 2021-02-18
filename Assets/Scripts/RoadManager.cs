23c500934ecd586ab06e646947f375c05760df1e
Chezzyz <48602862+Chezzyz@users.noreply.github.com>
2021-02-18 18:44:58 +0500

    1000 - 7...
    АХАХХАХАХАХАХАХХАХ
    я гуль..

---
+++ Assets/Scripts/RoadManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class RoadManager : MonoBehaviour
+{
+    [Range(0,100)] public float roadSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    public void Move()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.back * roadSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RoadSpawner : MonoBehaviour
+{
+    private List<GameObject> roadsGO;
+    [SerializeField] GameObject TestRoad;
+    private GameObject roadPref;
+    [Range(1,5)] public float roadCD = 3f;
+    private Transform SpawnPoint;
+
+
+    void Start()
+    {
+        SpawnPoint = transform;
+        StartCoroutine(Spawn());
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(roadCD);
+        var newRoad = Instantiate(TestRoad,SpawnPoint);
+        newRoad.transform.localPosition = new Vector3(transform.position.x, transform.position.y, 1);
+        newRoad.GetComponent<RoadManager>().Move();
+        StartCoroutine(Spawn());
+    }
+    void Move()
+    {
+        //testRoad.transform.DOLocalMoveX(testRoad.transform.position.x - 1, 3f);
+    }
+}
