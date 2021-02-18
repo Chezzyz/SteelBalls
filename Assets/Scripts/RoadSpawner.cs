0a22f2802b5bcbcf67b0e8601655ce0df79fa691
Chezzyz <48602862+Chezzyz@users.noreply.github.com>
2021-02-18 23:39:34 +0500

    Сделал спаун дорог

--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Linq;
 
 public class RoadSpawner : MonoBehaviour
 {
     private List<GameObject> roadsGO;
+    private int currentRoad;
     [SerializeField] GameObject TestRoad;
     private GameObject roadPref;
-    [Range(1,5)] public float roadCD = 3f;
+    [Range(1,40)] public float roadCD;
     private Transform SpawnPoint;
 
 
     void Start()
     {
+        currentRoad = 0;
         SpawnPoint = transform;
+        FillRoadsList(1);
         StartCoroutine(Spawn());
+    }
+
+    private void FillRoadsList(int level)
+    {
+        string levelNum = $"Level_{level}";
+        roadsGO = Resources.LoadAll<GameObject>($"{levelNum}/RoadPrefabs").ToList();
+        foreach (var item in roadsGO)
+            Debug.Log(item.name);
     }
 
     private void OnEnable()
@@ -23,16 +35,25 @@ public class RoadSpawner : MonoBehaviour
         
     }
 
-    IEnumerator Spawn()
+    private IEnumerator Spawn()
     {
         yield return new WaitForSeconds(roadCD);
-        var newRoad = Instantiate(TestRoad,SpawnPoint);
-        newRoad.transform.localPosition = Vector3.zero;
-        newRoad.GetComponent<RoadManager>().Move();
-        StartCoroutine(Spawn());
+        if (currentRoad < roadsGO.Count)
+        {
+            var newRoad = Instantiate(roadsGO[currentRoad], SpawnPoint);
+            newRoad.transform.localPosition = Vector3.zero;
+            newRoad.GetComponent<RoadManager>().Move();
+            currentRoad++;
+            StartCoroutine(Spawn());
+            StartCoroutine(Suicide(newRoad));
+        }
+        else
+            Debug.Log("Roads list is empty!");
     }
-    void Move()
+
+    private IEnumerator Suicide(GameObject road)
     {
-        //testRoad.transform.DOLocalMoveX(testRoad.transform.position.x - 1, 3f);
+        yield return new WaitForSeconds(3*roadCD);
+        Destroy(road);
     }
 }
