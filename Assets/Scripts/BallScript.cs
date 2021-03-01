000d440396c0b41f873ef3725762826e3edf6c14
Chezzyz <48602862+Chezzyz@users.noreply.github.com>
2021-03-01 19:10:18 +0500

    Намазал

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [Range(-1,1)] public float force;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.up * Time.fixedDeltaTime * force);
+        //if(transform.position.)
+    }
+}
