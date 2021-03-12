a76771054bf7f7cb7c195829ff0c44a2472ec41d
Denis <denissalyhov@gmail.com>
2021-03-12 23:11:46 +0500

    Reborn

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -10,17 +10,21 @@ public class ControllerScript : MonoBehaviour
     private Vector3 dragOrigin;
     private new Camera camera;
     private Vector3 startRot;
-    private Vector3 rotDiff;
-
+    private Vector3 rotDiff = Vector3.zero;
+    private float angle => (15f + ((rotDiff.y - rotRangeMin) / (rotRangeMax - rotRangeMin)) * 65f) * Mathf.PI/180f;
+    [Range(-1,0)] public float rotRangeMin = -0.15f; 
+    [Range(-1,0)] public float rotRangeMax = -0.45f; 
     [SerializeField] public new Rigidbody rigidbody;
     [SerializeField] public float sensetive;
     [SerializeField] public float jumpForce;
+    [SerializeField] public float radius = 4f;
     [SerializeField] public GameObject cameraGO;
     [SerializeField] public GameObject target;
     [SerializeField] public Animator animator;
     private GameObject JabkaGO => gameObject;
     private Transform CameraTransform => camera.transform;
     private Transform JabkaTransform => JabkaGO.transform;
+
 
     void Start()
     {
@@ -36,16 +40,16 @@ public class ControllerScript : MonoBehaviour
             draggingFlag = true;
             dragOrigin = camera.ScreenToViewportPoint(Input.mousePosition);
             startRot = transform.localEulerAngles;
-            rigidbody.freezeRotation = false;
         }
 
         if (Input.GetMouseButton(0) && draggingFlag)
         {
+            print(angle);
             rotDiff = camera.ScreenToViewportPoint(Input.mousePosition) - dragOrigin;
             transform.localEulerAngles = new Vector3(0,startRot.y - rotDiff.x*sensetive,0);
         }
 
-        if (rotDiff.y < -0.15f)
+        if (rotDiff.y < rotRangeMin)
         {
             animator.SetBool("JumpPrep", true);
             StartCoroutine(UIManager.singleton?.ChangeVignetteState(0, true));
@@ -58,13 +62,15 @@ public class ControllerScript : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-            if (rotDiff.y < -0.15f)
-            {
+            if (rotDiff.y < rotRangeMin)
+            { 
+                print(target.transform.localPosition);
+                target.transform.localPosition = new Vector3(0, Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius);
+                jumpForce = Mathf.Abs(rotDiff.y * 10 / Mathf.Tan(angle));
                 rigidbody.AddForce((target.transform.position - transform.position) * jumpForce, ForceMode.Impulse);
                 animator.SetBool("JumpPrep", false);
             }
             draggingFlag = false;
-            rigidbody.freezeRotation = true;
             rotDiff = Vector3.zero;
         }
     }
